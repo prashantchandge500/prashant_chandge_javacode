2f9c3ad5bd5831791de7ac6acd8461ad649b93d3
Prashant chandge <106652706+prashantchandge500@users.noreply.github.com>
2022-06-01 13:40:05 +0530

    Add files via upload

---
+++ JDBCSQL_Project/src/demo_jdbc/Jdbcpostgres_demo.java
@@ -0,0 +1,55 @@
+package demo_jdbc;
+
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+
+public class Jdbcpostgres_demo {
+
+    public static void main(String[] args) {
+        Connection con = null;
+        Statement stmt = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+            con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/employee_data", "postgres", "root");
+            con.setAutoCommit(false);
+            System.out.println("database connection opened");
+
+            /*stmt=con.createStatement();
+            String sql="CREATE TABLE STUDENT_RECORD" + "(ROLL_NO SERIAL NOT NULL PRIMARY KEY,"
+                    + "NAME TEXT NOT NULL," + "MOBILE_NO BIGINT NOT NULL," + 
+                    "ADDRESS TEXT NOT NULL," + 
+                    "PERCENTAGE REAL NOT NULL)";
+            stmt.executeUpdate(sql);*/
+            stmt = con.createStatement();
+            /* String sql="INSERT INTO STUDENT_RECORD(ROLL_NO,NAME,MOBILE_NO,ADDRESS,PERCENTAGE)" + "VALUES(4,'VISHAL BADE',8545129687,'Aurangabad',78)";
+            stmt.executeUpdate(sql);
+            sql="INSERT INTO STUDENT_RECORD(ROLL_NO,NAME,MOBILE_NO,ADDRESS,PERCENTAGE)" + "VALUES(5,'SHUBHAM CHILWANT',9456865239,'Aurangabad',85)";
+            stmt.executeUpdate(sql);*/
+            ResultSet rs = stmt.executeQuery(" select * from student_record;");
+            while (rs.next()) {
+                int roll_no = rs.getInt("ROLL_NO");
+                String name = rs.getString("NAME");
+                String mobli_no = rs.getString("MOBILE_NO");
+                String address = rs.getString("ADDRESS");
+                double percentage = rs.getDouble("PERCENTAGE");
+
+                System.out.println("Roll_no: " + roll_no);
+                System.out.println("NAME: " + name);
+                System.out.println("MOBILE_NO: " + mobli_no);
+                System.out.println("ADDRESS: " + address);
+                System.out.println("PERCENTAGE: " + percentage);
+
+            }
+            stmt.close();
+            con.commit();
+            con.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println(e.getClass().getName());
+            System.exit(0);
+        }
+        System.out.println("values inserted in table successfully..");
+    }
+}
